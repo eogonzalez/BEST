403b11ce6f96067c594d4bc8ca543328d8c56355
Eder Gonzalez <djgonzalezeder@gmail.com>
2015-08-03 22:00:07 -0600

    * Se agregaron paquetes de Owin para la seguridad del LOgin * Se agrego LoginModel.cs * Se agrego Startup.cs y Startup.Auth.cs para el manejo de la autenticacion

---
+++ BEST/Models/LoginModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace BEST.Models
+{
+    public class RegistroModel
+    {
+        public string nombre_empresa { get; set; }
+        public string usuario { get; set; }
+        public string correo { get; set; }
+
+        public string contraseña { get; set; }
+
+        public string confirmarcontraseña { get; set; }
+
+    }
+}
---
+++ BEST/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BEST.Startup))]
+namespace BEST
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
